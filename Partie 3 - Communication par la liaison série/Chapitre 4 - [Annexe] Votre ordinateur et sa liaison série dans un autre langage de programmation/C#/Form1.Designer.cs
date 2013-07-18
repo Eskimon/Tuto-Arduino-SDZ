7bc4c608e370ddeec3afbe62ef4732cef850084b
eskimon <simon.landrault@live.fr>
2013-07-18 16:21:00 +0200

    Ajout de la partie C#

---
+++ Partie 3 - Communication par la liaison série/Chapitre 4 - [Annexe] Votre ordinateur et sa liaison série dans un autre langage de programmation/C#/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace SdZSerialArduino
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboPort = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboVitesse = new System.Windows.Forms.ComboBox();
+            this.btnConnexion = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.boxEmission = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.boxReception = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Port : ";
+            // 
+            // comboPort
+            // 
+            this.comboPort.FormattingEnabled = true;
+            this.comboPort.Location = new System.Drawing.Point(55, 13);
+            this.comboPort.Name = "comboPort";
+            this.comboPort.Size = new System.Drawing.Size(121, 21);
+            this.comboPort.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(183, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Vitesse : ";
+            // 
+            // comboVitesse
+            // 
+            this.comboVitesse.FormattingEnabled = true;
+            this.comboVitesse.Location = new System.Drawing.Point(240, 13);
+            this.comboVitesse.Name = "comboVitesse";
+            this.comboVitesse.Size = new System.Drawing.Size(121, 21);
+            this.comboVitesse.TabIndex = 3;
+            // 
+            // btnConnexion
+            // 
+            this.btnConnexion.Location = new System.Drawing.Point(367, 12);
+            this.btnConnexion.Name = "btnConnexion";
+            this.btnConnexion.Size = new System.Drawing.Size(87, 23);
+            this.btnConnexion.TabIndex = 4;
+            this.btnConnexion.Text = "Connecter";
+            this.btnConnexion.UseVisualStyleBackColor = true;
+            this.btnConnexion.Click += new System.EventHandler(this.btnConnexion_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Emission";
+            // 
+            // boxEmission
+            // 
+            this.boxEmission.AcceptsReturn = true;
+            this.boxEmission.Location = new System.Drawing.Point(16, 62);
+            this.boxEmission.Multiline = true;
+            this.boxEmission.Name = "boxEmission";
+            this.boxEmission.Size = new System.Drawing.Size(438, 101);
+            this.boxEmission.TabIndex = 6;
+            this.boxEmission.TextChanged += new System.EventHandler(this.boxEmission_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 170);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Réception";
+            // 
+            // boxReception
+            // 
+            this.boxReception.AcceptsReturn = true;
+            this.boxReception.Location = new System.Drawing.Point(16, 186);
+            this.boxReception.Multiline = true;
+            this.boxReception.Name = "boxReception";
+            this.boxReception.ReadOnly = true;
+            this.boxReception.Size = new System.Drawing.Size(438, 101);
+            this.boxReception.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(476, 311);
+            this.Controls.Add(this.boxReception);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.boxEmission);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnConnexion);
+            this.Controls.Add(this.comboVitesse);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboPort);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "SdZ terminal voie série";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboPort;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboVitesse;
+        private System.Windows.Forms.Button btnConnexion;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox boxEmission;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox boxReception;
+    }
+}
+
---
+++ Partie 3 - Communication par la liaison série/Chapitre 4 - [Annexe] Votre ordinateur et sa liaison série dans un autre langage de programmation/C#/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace SdZSerialArduino
+{
+    public partial class Form1 : Form
+    {
+        SerialPort port;
+
+        //une déléguée pour pouvoir mettre à jour le texte de la boite de réception de manière "thread-safe"
+        delegate void SetTextCallback(string text);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //on commence par lister les voies séries présentes
+            String[] ports = SerialPort.GetPortNames(); //fonction statique
+            //on ajoute les ports au combo box
+            foreach (String s in ports)
+                comboPort.Items.Add(s);
+
+            //on ajoute les vitesses au combo des vitesses
+            comboVitesse.Items.Add("300");
+            comboVitesse.Items.Add("1200");
+            comboVitesse.Items.Add("2400");
+            comboVitesse.Items.Add("4800");
+            comboVitesse.Items.Add("9600");
+            comboVitesse.Items.Add("14400");
+            comboVitesse.Items.Add("19200");
+            comboVitesse.Items.Add("38400");
+            comboVitesse.Items.Add("57600");
+            comboVitesse.Items.Add("115200");
+        }
+
+        private void btnConnexion_Click(object sender, EventArgs e)
+        {
+            //on gère la connexion/déconnexion
+            if (btnConnexion.Text == "Connecter") //alors on connecte
+            {
+                //crée un nouvel objet voie série
+                port = new SerialPort();
+                //règle la voie série
+                port.BaudRate = int.Parse(comboVitesse.SelectedItem.ToString()); //parse en int le combo des vitesses
+                port.DataBits = 8;
+                port.StopBits = StopBits.One;
+                port.Parity = Parity.None;
+                port.PortName = comboPort.SelectedItem.ToString(); //récupère le nom sélectionné
+
+                //ajoute un gestionnaire de réception pour la réception de donnée sur la voie série
+                port.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+
+                port.Open(); //ouvre la voie série
+
+                btnConnexion.Text = "Deconnecter";
+            }
+            else //sinon on déconnecte
+            {
+                port.Close(); //ferme la voie série
+                btnConnexion.Text = "Connecter";
+            }
+        }
+
+        private void boxEmission_TextChanged(object sender, EventArgs e)
+        {
+            //met le dernier caractère dans un tableau avec une seule case le contenant
+            char[] car = new char[] { boxEmission.Text[boxEmission.TextLength - 1] };
+            if (port != null && port.IsOpen) //on s'assure que le port est existant et ouvert
+                port.Write(car, 0, 1); //envoie le tableau de caractère, depuis la position 0, et envoie 1 seul élément
+        }
+
+        //gestionnaire de la réception de caractère
+        private void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            String texte = port.ReadExisting();
+            SetText(texte);
+        }
+        
+        private void SetText(string text)
+        {
+            if (boxReception.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText);
+                boxReception.Invoke(d, new object[] { text });
+            }
+            else
+                boxReception.Text += text;
+        }
+    }
+}
