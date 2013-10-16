71f9d48c21d85cdf330444bf2a74c7629b96d93c
Eskimon <eskimon@outlook.com>
2013-10-16 22:38:39 +0200

    Nouveaux schémas pour les capteurs

---
+++ Partie 5 - Les capteurs et l'environnement autour d'Arduino/Chapitre 2 - Différents types de mesure/LM35.ino
@@ -0,0 +1,23 @@
+﻿const int capteur = 0; //capteur branché sur la pin 0
+float tension = 0.0;
+int valeur = 0;
+
+void setup()
+{
+    Serial.begin(9600);
+}
+
+void loop()
+{
+    valeur = analogRead(capteur);
+    tension = (valeur*5)/1024;
+    
+    Serial.print("Tension : ");
+    Serial.print(tension);
+    Serial.println(" V");
+    Serial.print("Valeur : ");
+    Serial.println(valeur);
+    Serial.println("---------------------");
+    
+    delay(500);
+}
